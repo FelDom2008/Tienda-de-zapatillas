a0d3169e3bd319187217cca91262ae92218741b0
julitabonita <149689096+julitabonita@users.noreply.github.com>
2023-11-09 11:23:20 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,9 @@
-var nombre = document.getElementById("nom")
+var nombre = document.getElementById("nom").value                                             
+var apellido = document.getElementById("ape").value
+var direccion = document.getElementById("direc").value
+
+if(nombre == "" || apellido == "" || direccion== ""){
+  alert('Completar los campos vacios');
+} 
+
+
